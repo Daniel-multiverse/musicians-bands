1e26a5fb6978dcd00a73e4947a388ec5c6aad103
Daniel-multiverse <daniel.oyewole@multiverse.io>
2023-02-15 15:33:50 +0000

    define the Band.js models

--- Band.js
+++ Band.js
@@ -1,7 +1,14 @@
 const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Band model
-let Band;
+let Band = sequelize.define("Band", {
+    name: {
+        type: Sequelize.STRING,
+      },
+      genre: {
+        type: Sequelize.STRING,
+      }
+})
 
 module.exports = {
     Band
